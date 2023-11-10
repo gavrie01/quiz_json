c247a720458f09e73b75663a537779def54ae8d8
elena <elena.e.gav@gmail.com>
2023-11-10 22:37:14 +0100

    fixed shuffling, sidebar and counter added, questions are added

--- quiz.py
+++ quiz.py
@@ -2,6 +2,10 @@ import streamlit as st
 import json
 import random
 
+correct_answers_counter = 0 # add to st.sidebar
+
+
+
 st.title("Quiz ☕")
 
 uploaded_file = st.file_uploader("Upload your json or select existing in 'data/' folder", type=["json"])
@@ -9,12 +13,19 @@ uploaded_file = st.file_uploader("Upload your json or select existing in 'data/'
 if uploaded_file:
     try:
         quiz_data = json.load(uploaded_file)
-        random.shuffle(quiz_data)
-        for idx, question_data in enumerate(quiz_data, start=1):
+
+        # Check if the order has been shuffled before
+        if "shuffled_order" not in st.session_state:
+            # If not, shuffle the order and store it in the session state
+            st.session_state.shuffled_order = list(range(len(quiz_data)))
+            random.shuffle(st.session_state.shuffled_order)
+
+        for idx, question_idx in enumerate(st.session_state.shuffled_order, start=1):
+            question_data = quiz_data[question_idx]
             question = question_data["question"]
             options = question_data["options"]
             correct_answer = question_data["correct_answer"]
-            image_path = question_data.get("image_path", None)  # Get the image path (if available)
+            image_path = question_data.get("image_path", None)
 
             # Display the question with Markdown (including an image if available)
             st.markdown(f"**Question {idx}:** {question}", unsafe_allow_html=True)
@@ -24,9 +35,14 @@ if uploaded_file:
 
             if user_answer:
                 if user_answer == correct_answer:
-                #if all(answer in correct_answers for answer in user_answers):
+                    correct_answers_counter += 1
                     st.success("Correct!")
                 else:
                     st.error(f"The correct answer is: {correct_answer}")
+        # Display the correct answers counter in the sidebar
+        st.sidebar.header("Correct / Total")
+        st.sidebar.write(correct_answers_counter,"/", len(quiz_data))
+
+
     except json.JSONDecodeError:
         st.error("Invalid JSON file. Please upload a valid JSON file.")
